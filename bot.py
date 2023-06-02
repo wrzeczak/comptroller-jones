30ca971545670f9513ee0624a0323ff85b4e3185
LividPhysics <wrzeczak@protonmail.com>
2023-06-02 13:23:39 -0500

    fix it

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ intents.message_content = True
 client = discord.Client(intents=intents)
 
 @client.event
-async def on_ready()
+async def on_ready():
     print(f'{client.user} logged on at {time.ctime(time.time())} (UTC).')
 
 @client.event
